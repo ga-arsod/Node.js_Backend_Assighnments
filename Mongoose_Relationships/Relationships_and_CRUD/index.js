e05718278bc196a995efc9c620e1b56c59a5c2fb
ga-arsod <gaarsod14@gmail.com>
2022-03-13 22:19:10 +0530

    Update index.js

--- Mongoose_Relationships/Relationships_and_CRUD/index.js
+++ Mongoose_Relationships/Relationships_and_CRUD/index.js
@@ -11,248 +11,174 @@ const connect = () => {
     );
 };
 
-//section schema
-//1. creating section schema
+// 1. user schema 
+const userSchema = new mongoose.Schema(
+    {
+        firstName : {type: String, required: true},
+        lastName : {type: String, required : true}
+    },
+    {
+        versionKey: false,
+        timestamps: true
+    }
+);
+
+const User = mongoose.model("user", userSchema);
+
+
 const sectionSchema = new mongoose.Schema(
     {
         name : {type: String, required: true}
     },
     {
-        versionKey : false,
-        timestamps : true
+        versionKey: false,
+        timestamps: true
     }
-);
-
-// 2. creating model
+)
 const Section = mongoose.model("section", sectionSchema);
 
 
-// Books schema 
-// 1. creating books schema
-const booksSchema = new mongoose.Schema(
+const bookSchema = new mongoose.Schema(
     {
-        name : {type: String, required: true},
-        body : {type: String, required: true},
-        // section : {
-        //     type: mongoose.Schema.Types.ObjectId,
-        //     ref: "section",
-        //     required: true
-        // }
+        sectionID : {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "section",
+            required: true
+        },
+        name: {type: String, required: true},
+        body: {type: String, required: true}
     },
     {
-        versionKey : false,
-        timestamps : true
+        versionKey: false,
+        timestamps: true
     }
-);
-
-// 2. creating model
-const Book = mongoose.model("book", booksSchema);
+)
+const Book = mongoose.model("book", bookSchema);
 
 
-// Authors Schema
-// 1. creating author schema 
-const authorSchema = new mongoose.Schema( 
+const authorSchema = mongoose.Schema(
     {
-        first_name : {type: String, required: true},
-        last_name : {type: String, required: false},
-        // book : {
-        //     type: mongoose.Schema.Types.ObjectId,
-        //     ref: "book",
-        //     required: true
-        // }
+        userID : {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user",
+            required: true
+        }
     },
     {
-        versionKey : false,
-        timestamps : true
+        versionKey: false,
+        timestamps: true
     }
-);
-
-// 2. author model
+)
 const Author = mongoose.model("author", authorSchema);
 
 
-// assignment conditions
-// 1 Condition - find all books written by an author
-app.get("/authors/:id", async (req, res) => {
-    try {
-        const author = await Author.findById(req.params.id).lean().exec();
+const book_authorSchema = mongoose.Schema (
+    {
+        authorID : {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "author",
+            required: true
+        },
+        bookID : {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "book",
+            required: true
+        }
+    },
+    {
+        versionKey: false,
+        timestamps: true
+    }
+)
+const BookAuthor = mongoose.model("book_author", book_authorSchema);
 
-        return res.status(200).send({author: author});
+
+const checkedOutSchema = mongoose.Schema(
+    {
+        userID : {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user",
+            required: true
+        },
+        bookID : {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "book",
+            required: true
+        },
+        checkeOutTime : {type: String},
+        checkedInTime : {type: String}
+    },
+    {
+        versionKey: false,
+        timestamps: true
+    }
+)
+const CheckAvailability = mongoose.model("checkAvailability", checkedOutSchema);
+
+
+//find all books written by an author
+app.get("/book_authors", async (req, res) => {
+    try{
+        const book_author = await BookAuthor.find({}).populate(authorID).lean().exec();
+
+        return res.status(200).send(book_author);
     }
     catch(err) {
-        return res.status(500).send({message: err.message})
+        return res.status(500).send(err)
+    }
+}) 
+
+
+//find books in a section
+app.get("/books/:id", async (req, res) => {
+    try{
+        const books = await BookAuthor.find({sectionID: {$eq : id}}).populate(sectionID).lean().exec();
+
+        return res.status(200).send(books);
+    }
+    catch(err) {
+        return res.status(500).send(err);
     }
 })
 
 
-// 2 condition - find books in a section
-app.get("/sections/:id", async (req, res) => {
-    try {
-        const section = await Section.findById(req.params.id).lean().exec();
+//find books in a section that are not checked out
+app.get("/available", async(req, res) => {
+    try{
+        const available = await CheckAvailability.find({$or: [{checkeOutTime: {$eq : null}}, {checkeOutTime: {$lt : checkedInTime}}]});
 
-        return res.status(200).send({section: section});
+        return res.status(200).send(available);
     }
     catch(err) {
-        return res.status(500).send({message: err.message})
+        return res.status(500).send(err);
     }
 })
 
-// 3 condition - find books in a section that are not checked out
-// 4 condition - find books of 1 author inside a section Optional
-// 5 condition - find books that are checked out ( This is optional as if you skipped the CheckedOut model then you will not be able to do this )
+
+//find books of 1 author inside a section Optional
+app.get("/book_authors/:id", async (req, res) => {
+    try{
+        const book_author = await BookAuthor.find({authorID: {$eq : id}}).populate(bookID).lean().exec();
+
+        return res.status(200).send(book_author);
+    }
+    catch(err) {
+        return res.status(500).send(err)
+    }
+}) 
 
 
+//find books that are checked out ( This is optional as if you skipped the CheckedOut model then you will not be able to do this )
+app.get("/available", async(req, res) => {
+    try{
+        const available = await CheckAvailability.find({$and: [{checkeOutTime: {$ne : null}}, {checkeOutTime: {$gt : checkedInTime}}]});
 
-// the below 
-
-// posting section, books and authors data
-// app.post("/sections", async (req, res) => {
-//     try {
-//         const section = await Section.create(req.body)
-
-//         return res.status(201).send(section);
-//     }
-//     catch(err) {
-//         return res.status(500).send({message: err.message})
-//     }
-// })
-
-// app.post("/books", async (req, res) => {
-//     try {
-//         const book = await Book.create(req.body)
-
-//         return res.status(201).send(book);
-//     }
-//     catch(err) {
-//         return res.status(500).send({message: err.message})
-//     }
-// })
-
-// app.post("/authors", async (req, res) => {
-//     try {
-//         const author = await Author.create(req.body)
-
-//         return res.status(201).send(author);
-//     }
-//     catch(err) {
-//         return res.status(500).send({message: err.message})
-//     }
-// })
-
-
-// //getting section, authors and books data by their id's
-// app.get("/sections/:id", async (req, res) => {
-//     try {
-//         const section = await Section.findById(req.params.id).lean().exec();
-
-//         return res.status(200).send({section: section});
-//     }
-//     catch(err) {
-//         return res.status(500).send({message: err.message})
-//     }
-// })
-
-// app.get("/books/:id", async (req, res) => {
-//     try {
-//         const book = await Book.findById(req.params.id).lean().exec();
-
-//         return res.status(200).send({book: book});
-//     }
-//     catch(err) {
-//         return res.status(500).send({message: err.message})
-//     }
-// })
-
-// app.get("/authors/:id", async (req, res) => {
-//     try {
-//         const author = await Author.findById(req.params.id).lean().exec();
-
-//         return res.status(200).send({author: author});
-//     }
-//     catch(err) {
-//         return res.status(500).send({message: err.message})
-//     }
-// })
-
-
-// // updating section, books and authors data by their id's
-// app.patch("/sections/:id", async(req, res) => {
-//     try {
-//         const section = await Section.findByIdAndUpdate(req.params.id, req.body, {
-//             new: true
-//         }).lean().exec();
-
-//         return res.status(201).send({section: section})
-//     }
-//     catch(err) {
-//         return res.status(500).send({message: err.message});
-//     }
-// })
-
-// app.patch("/books/:id", async(req, res) => {
-//     try {
-//         const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
-//             new: true
-//         }).lean().exec();
-
-//         return res.status(201).send({book: book})
-//     }
-//     catch(err) {
-//         return res.status(500).send({message: err.message});
-//     }
-// })
-
-// app.patch("/authors/:id", async(req, res) => {
-//     try {
-//         const author = await Author.findByIdAndUpdate(req.params.id, req.body, {
-//             new: true
-//         }).lean().exec();
-
-//         return res.status(201).send({author: author})
-//     }
-//     catch(err) {
-//         return res.status(500).send({message: err.message});
-//     }
-// })
-
-
-// //deleting the section or books or authors document
-
-// app.delete("/section/:id", async(req, res) => {
-//     try{
-//         const section = await Section.findByIdAndDelete(req.params.id).lean().exec();
-
-//         return res.status(200).send(section)
-//     }
-//     catch(err) {
-//         return res.status(500).send({message: err.message});
-//     }
-// })
-
-// app.delete("/books/:id", async(req, res) => {
-//     try{
-//         const book = await Book.findByIdAndDelete(req.params.id).lean().exec();
-
-//         return res.status(200).send(book)
-//     }
-//     catch(err) {
-//         return res.status(500).send({message: err.message});
-//     }
-// })
-
-// app.delete("/authors/:id", async(req, res) => {
-//     try{
-//         const author = await Author.findByIdAndDelete(req.params.id).lean().exec();
-
-//         return res.status(200).send(author)
-//     }
-//     catch(err) {
-//         return res.status(500).send({message: err.message});
-//     }
-// })
-
-
-
+        return res.status(200).send(available);
+    }
+    catch(err) {
+        return res.status(500).send(err);
+    }
+})
 
 
 app.listen(5500, async () => {
